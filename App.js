abcee17e48736871cbb8b75d902cb2d8d4bdea3f
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2024-06-06 18:57:16 +0530

    Added star background (because i am a star)

--- App.js
+++ App.js
@@ -1,57 +1,30 @@
+
+//star generator
 document.addEventListener('DOMContentLoaded', function() {
-    const numDots = 60; // Number of dots to create
+    const numDots = 200;
     const body = document.body;
 
     for (let i = 0; i < numDots; i++) {
-        const dot = document.createElement('div');
+        const dot = document.createElement('img');
         dot.classList.add('dot');
+        dot.src = "/img/star.png";
 
-        // Random position
         const posX = Math.random() * window.innerWidth;
         const posY = Math.random() * window.innerHeight;
+        const rotate = Math.random() * 90
 
+        dot.style.rotate = `${rotate}deg`
         dot.style.left = `${posX}px`;
         dot.style.top = `${posY}px`;
-        const size = Math.floor(Math.random() * (12 - 2 + 1)) + 2;
+        const size = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
         dot.style.width = `${size}px`;
         dot.style.height = `${size}px`;
         console.log('Size:', size);
         body.appendChild(dot);
     }
 
-    // Reorder dots in DOM to ensure they appear behind other content
     const dots = document.querySelectorAll('.dot');
     dots.forEach(dot => body.insertBefore(dot, body.firstChild));
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.querySelector('.container');
-    const dots = document.querySelectorAll('.dot');
-
-    container.addEventListener('mousemove', function(event) {
-        const containerRect = container.getBoundingClientRect();
-        const containerX = containerRect.left;
-        const containerY = containerRect.top;
-
-        const mouseX = event.clientX - containerX;
-        const mouseY = event.clientY - containerY;
-
-        dots.forEach(dot => {
-            const rect = dot.getBoundingClientRect();
-            const dotCenterX = rect.left + rect.width / 2;
-            const dotCenterY = rect.top + rect.height / 2;
-
-            const dx = mouseX - dotCenterX;
-            const dy = mouseY - dotCenterY;
-
-            // Calculate distance from dot to mouse
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            dot.style.posX = dx
-            dot.style.posY = dy
-
-        });
-    });
-});
-
 
 
