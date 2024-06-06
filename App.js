817497e31838a8a3349d091687977ed8bf99f104
MohitTiwariBytes <mohittiwaridev@gmail.com>
2024-06-06 19:48:04 +0530

    added it

--- App.js
+++ App.js
@@ -1,33 +1,31 @@
-
 //star generator
-document.addEventListener('DOMContentLoaded', function() {
-    const numDots = window.innerHeight * window.innerWidth / 10000;
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  const numDots = (window.innerHeight * window.innerWidth) / 10000;
+  const body = document.body;
 
-    for (let i = 0; i < numDots; i++) {
-        const dot = document.createElement('img');
-        dot.classList.add('dot');
-        dot.src = "/img/star.png";
+  for (let i = 0; i < numDots; i++) {
+    const dot = document.createElement("img");
+    dot.classList.add("dot");
+    dot.src = "/img/star.png";
 
-        const posX = Math.random() * window.innerWidth;
-        const posY = Math.random() * window.innerHeight;
-        const rotate = Math.random() * 90
+    const posX = Math.random() * window.innerWidth;
+    const posY = Math.random() * window.innerHeight;
+    const rotate = Math.random() * 90;
 
-        dot.style.rotate = `${rotate}deg`
-        dot.style.left = `${posX}px`;
-        dot.style.top = `${posY}px`;
-        const size = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
-        dot.style.width = `${size}px`;
-        dot.style.height = `${size}px`;
-        console.log('Size:', size);
-        body.appendChild(dot);
-    }
+    dot.style.rotate = `${rotate}deg`;
+    dot.style.left = `${posX}px`;
+    dot.style.top = `${posY}px`;
+    const size = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
+    dot.style.width = `${size}px`;
+    dot.style.height = `${size}px`;
+    console.log("Size:", size);
+    body.appendChild(dot);
+  }
 
-    const dots = document.querySelectorAll('.dot');
-    dots.forEach(dot => body.insertBefore(dot, body.firstChild));
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot) => body.insertBefore(dot, body.firstChild));
+
+  body.addEventListener("mousemove", (event) => {
+    console.log(event.pageX);
+  });
 });
-
-
-function moveStars(e){
-    console.log(e.posX)
-}
