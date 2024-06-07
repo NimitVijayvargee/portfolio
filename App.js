4ccc7df8b7660ea9ba05415231dd2627054824e7
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2024-06-07 22:27:18 +0530

    Background work

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // star generator
 document.addEventListener('DOMContentLoaded', function() {
-    const numDots = window.innerHeight * window.innerWidth / 10000;
+    const numDots = window.innerHeight * window.innerWidth / 5000;
     const body = document.body;
 
     // Set body dimensions to ensure stars stay within it
@@ -24,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
         dot.style.top = `${posY}px`;
         dot.setAttribute('data-initial-left', posX);
         dot.setAttribute('data-initial-top', posY);
-        dot.setAttribute('data-multiplier', Math.random() / 10);  // Adjust multiplier for effect
-        const size = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
+        dot.setAttribute('data-multiplier', (Math.random()));  // Adjust multiplier for effect
+        const size = Math.floor(Math.random() * (15 - 5 + 1)) + 2;
         dot.style.width = `${size}px`;
         dot.style.height = `${size}px`;
         console.log('Size:', size);
