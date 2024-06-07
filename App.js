7bedde136b3b0f5ef361c2875be4e4fe09309154
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2024-06-07 16:45:01 +0530

    parallax

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-
-//star generator
 document.addEventListener('DOMContentLoaded', function() {
     const numDots = window.innerHeight * window.innerWidth / 10000;
     const body = document.body;
+    
+    const docWidth = Math.max(document.documentElement.scrollWidth, window.innerWidth);
+    const docHeight = Math.max(document.documentElement.scrollHeight, window.innerHeight);
+    body.style.width = `${docWidth}px`;
+    body.style.height = `${docHeight}px`;
 
     for (let i = 0; i < numDots; i++) {
         const dot = document.createElement('img');
         dot.classList.add('dot');
         dot.src = "/img/star.png";
 
-        const posX = Math.random() * window.innerWidth;
-        const posY = Math.random() * window.innerHeight;
-        const rotate = Math.random() * 90
+        const posX = Math.random() * docWidth;
+        const posY = Math.random() * docHeight;
+        const rotate = Math.random() * 90;
 
-        dot.style.rotate = `${rotate}deg`
+        dot.style.transform = `rotate(${rotate}deg)`;
+        dot.style.position = 'absolute';
         dot.style.left = `${posX}px`;
         dot.style.top = `${posY}px`;
+        dot.setAttribute('data-multiplier', Math.random() / 50);
         const size = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
         dot.style.width = `${size}px`;
         dot.style.height = `${size}px`;
@@ -27,20 +32,21 @@ document.addEventListener('DOMContentLoaded', function() {
     dots.forEach(dot => body.insertBefore(dot, body.firstChild));
 });
 
+window.addEventListener("scroll", () => {
+    const x = window.scrollX;
+    const y = window.scrollY;
+    console.log(x, y);
 
-/*function moveStars(e){
-    let x = e.clientX;
-    let y = e.clientY;
-    const dots = document.querySelectorAll('.dot');
-    dots.forEach(dot =>{
-        let dotx = dot.posX;
-        let doty = dot.posY;
-        let dx = dotx - x;
-        let dy = doty - y;
-        dot.style.left = `${dotx-dx}px`;
-        dot.style.top = `${dotx-dx}px`;
-        
-
+    const dots = document.getElementsByClassName('dot');
+    Array.from(dots).forEach(dot => {
+        parallax(dot, x, y);
     });
+});
+
+function parallax(dot, x, y) {
+    const multiplier = parseFloat(dot.getAttribute("data-multiplier"));
+    const posX = parseFloat(dot.style.left);
+    const posY = parseFloat(dot.style.top);
+    dot.style.left = `${posX - x * multiplier}px`;
+    dot.style.top = `${posY - y * multiplier}px`;
 }
-*/
