d3c7319a3d4b59b657530e5dbbcda870f0919c91
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2024-06-07 22:28:24 +0530

    changed star count

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // star generator
 document.addEventListener('DOMContentLoaded', function() {
-    const numDots = window.innerHeight * window.innerWidth / 5000;
+    const numDots = window.innerHeight * window.innerWidth / 10000;
     const body = document.body;
 
     // Set body dimensions to ensure stars stay within it
